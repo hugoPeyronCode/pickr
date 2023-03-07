7499b55d6a768518d6f71d3fa8560a6b13e756c5
Audrey Guigo <guigo.audrey@gmail.com>
2023-03-07 17:50:44 +0100

    added search bar from geoloc and form for deck.new

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -5,4 +5,26 @@ class DecksController < ApplicationController
 
   def choose
   end
+
+  def new
+    @deck = Deck.new
+    @deckitem = DeckItem.new
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+    @deck.user = current_user
+    @items = Item.near(params[:address], 0.5).limit(10)
+    if @deck.save!
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
+  end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,13 @@
 class Deck < ApplicationRecord
+  before_create :set_default_status
   belongs_to :user
   validates :name, presence: true
 
   has_many :deck_items, dependent: :destroy
+
+  private
+
+  def set_default_status
+    self.status = 'pending'
+  end
 end
