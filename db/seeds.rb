b7802072fed3599cc00b2d670494b55355bc171e
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-03-07 12:14:00 +0100

    premiere seed avec users et decks

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,15 @@ guillaume = User.create!(username: "Guillaume", email: "guillaume@hello.fr", pas
 audrey = User.create!(username: "Audrey", email: "audrey@hello.fr", password: "123456", phone: "06 32 45 67 89")
 hugo = User.create!(username: "Hugo", email: "hugo@hello.fr", password: "123456", phone: "06 12 34 56 78")
 robin = User.create!(username: "Robin", email: "robin@hello.fr", password: "123456", phone: "07 98 76 54 32")
-puts "users created"
+puts "Users created"
+
+puts "Creating 1 Pending Deck and 2 Closed Decks for Hugo..."
+restaurantdeck1 = Deck.create!(name: "hugo1-rest-230301", status: "Pending", user: hugo)
+restaurantdeck2 = Deck.create!(name: "hugo2-rest-230302", status: "Closed", user: hugo)
+restaurantdeck3 = Deck.create!(name: "hugo3-rest-230303", status: "Closed", user: hugo)
+puts "Decks created"
+
+# puts "Creating 5 Restaurants for Pending deck..."
+# puts "Restaurants created"
+# puts "Creating 5 Restaurants for Future deck..."
+# puts "Restaurants created"
