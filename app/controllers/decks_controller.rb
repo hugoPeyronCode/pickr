d7ddc7827bbfaad68c2d5ee9da3bcc159536c668
Robin SIX <robin.six@gmail.com>
2023-03-08 17:01:19 +0100

    swiper fonctionnel de ouf

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -16,7 +16,7 @@ class DecksController < ApplicationController
     @deck.user = current_user
     @items = Item.near(params[:address], 0.5).limit(10)
     if @deck.save!
-      redirect_to root_path
+      redirect_to deck_path(@deck)
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,6 +24,11 @@ class DecksController < ApplicationController
       deck_item = DeckItem.new(deck: @deck, item: item)
       deck_item.save!
     end
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+    @deck_items = @deck.deck_items
   end
 
   private
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import SwipeController from "./swipe_controller"
+application.register("swipe", SwipeController)
