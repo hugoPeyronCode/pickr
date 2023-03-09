58f356b1701caa6386a1746d098482bb9273f78a
Hugo Peyron <hugopeyron@gmail.com>
2023-03-09 12:27:35 +0100

    basic logic is done. Now a user can close a vote, it will change it's status and display view

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -17,6 +17,7 @@ class DecksController < ApplicationController
   def create
     @deck = Deck.new(deck_params)
     @deck.user = current_user
+    @deck.status = "Pending"
 
     if @deck.save!
       redirect_to deck_path(@deck)
@@ -37,6 +38,16 @@ class DecksController < ApplicationController
   def show
     @deck = Deck.find(params[:id])
     @deck_items = @deck.deck_items
+    @deck_winning_item = @deck.most_voted_deck_item
+  end
+
+  def close_vote
+    @deck = Deck.find(params[:id])
+    if @deck.update(status: "Closed")
+      redirect_to deck_path(@deck), notice: "Vote closed successfully."
+    else
+      redirect_to deck_path(@deck), alert: "Error closing vote."
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :decks, only: [:index, :new, :create, :show]
+  resources :decks, only: [:index, :new, :create, :show] do
+    member do
+      get :close_vote
+    end
+  end
   resources :deck_items, only: [] do
     resources :votes, only: [:create]
   end
