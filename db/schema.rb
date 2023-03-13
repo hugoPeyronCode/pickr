9c18240a578a528a631374f5affcbb797f70a5d6
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-03-13 15:37:31 +0100

    seed de 60 restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_144107) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_113639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_144107) do
     t.string "movie_director"
     t.text "synopsis"
     t.string "movie_genre"
+    t.string "opening_hours", array: true
   end
 
   create_table "users", force: :cascade do |t|
